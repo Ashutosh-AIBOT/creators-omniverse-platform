0d053ac247aca0e0a5fc00998db08a83f30bcb0c
Ashutosh-AIBOT <ashutoshknp0@email.com>
2026-04-21 14:42:53 +0530

    Fix ASGI initialization: Moved django.setup() before model imports

--- core/asgi.py
+++ core/asgi.py
@@ -1,13 +1,23 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import communities.routing
 
+# 1. Set settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# 2. Initialize Django apps early
+django.setup()
+
+# 3. Create HTTP application
+django_asgi_app = get_asgi_application()
+
+# 4. Import routing AFTER django.setup() to avoid AppRegistryNotReady
+import communities.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             communities.routing.websocket_urlpatterns
